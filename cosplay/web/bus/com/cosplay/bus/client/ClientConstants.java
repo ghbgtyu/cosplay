1d5407663f8f8bd829b63efbcb0ff33e5187c5ec
ghbgtyu <381904379@qq.com>
2016-02-16 10:02:53 +0800

    1

---
+++ cosplay/web/bus/com/cosplay/bus/client/ClientConstants.java
@@ -0,0 +1,9 @@
+package com.cosplay.bus.client;
+
+public class ClientConstants {
+	/**执行结果*/
+	public static final String RESULT = "result";
+	
+	/**错误码*/
+	public static final String ERROR_CODE = "code";
+}
